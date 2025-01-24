596431d95274dd0a68094d35ea54a9d72ea703fd
Christian Obando <alexander.obandoea@gmail.com>
2025-01-23 21:16:39 -0500

    se agrega la funcion para alimentar la lista

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombresAmigos = [];
+let i = 0;
 
 function agregarAmigo(){
 
     let nuevoNombre = document.querySelector('input').value;
     nuevoNombre ? nombresAmigos.push(nuevoNombre) : alert("debes escribir un nombre");
     document.querySelector('input').value = "";
-    console.log(nombresAmigos);
+    tablaNombres()
     
 }
+
+
+function tablaNombres(){
+    let tabla = document.getElementById('listaAmigos');
+    tabla.innerHTML ="";
+
+    for(i = 0 ; i < nombresAmigos.length ; i++){
+
+        let agregarNombre = document.createElement('li');
+        agregarNombre.textContent = nombresAmigos[i];
+        tabla.appendChild(agregarNombre)
+
+    }
+}
+
