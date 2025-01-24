0657215d37dc2118aab15161c734ad15978e3a6c
Christian Obando <alexander.obandoea@gmail.com>
2025-01-23 20:36:08 -0500

    se crea la funcion para registrar nombres

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+function agregarAmigo(){
+
+    let nuevoNombre = document.querySelector('input').value;
+    nuevoNombre ? nombresAmigos.push(nuevoNombre) : alert("debes escribir un nombre");
+    document.querySelector('input').value = "";
+    console.log(nombresAmigos);
+    
+}
